4bbc2d7baefe5f9f4f2f63ee4051678e6dddedd6
rliu23 <rebecca.liu.1c@gmail.com>
2026-04-08 23:51:34 -0500

    style

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -10,12 +10,24 @@ interface CourseListProps {
 }
 
 const CourseList = ({courses}: CourseListProps ) => (
-  <ul>
-  {
-    Object.entries(courses).map(([id, course]) => (
-      <li key={id}>{course.term} CS {course.number}: {course.title}</li>
-    ))
-  }
+  <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    {Object.entries(courses).map(([id, course]) => (
+      <li
+        key={id}
+        className="border rounded-sm p-4"
+      >
+        <div className="flex flex-col gap-2 h-full">
+          <div className="text-lg">
+            {course.term} CS {course.number}
+          </div>
+
+          <div className="flex-grow">{course.title}</div>
+
+          <div className="flex-grow border-t border-gray-400"></div>
+          {course.meets}
+        </div>
+      </li>
+    ))}
   </ul>
 );
 
